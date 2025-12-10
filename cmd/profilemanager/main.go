57be5cc82da8e53e9241b6c2b59df3d40105a39e
April <60031800+aprilrr@users.noreply.github.com>
2025-12-10 21:11:58 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/profilemanager/main.go
@@ -0,0 +1,23 @@
+// cmd/profilemanager/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "profilemanager/internal/profilemanager"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := profilemanager.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
